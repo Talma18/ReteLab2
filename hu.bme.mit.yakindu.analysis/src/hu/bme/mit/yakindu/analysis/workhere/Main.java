e557d4fb6d03753bca226d762ee18ca42fcc1594
cloud <cloud@remo>
2021-03-10 18:07:11 +0100

    2. feladat elvégzése

--- hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/Main.java
+++ hu.bme.mit.yakindu.analysis/src/hu/bme/mit/yakindu/analysis/workhere/Main.java
@@ -4,7 +4,10 @@ import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.junit.Test;
 import org.yakindu.sct.model.sgraph.State;
+import org.yakindu.sct.model.sgraph.Transition;
 import org.yakindu.sct.model.sgraph.Statechart;
+
+import java.util.ArrayList;
 
 import hu.bme.mit.model2gml.Model2GML;
 import hu.bme.mit.yakindu.analysis.modelmanager.ModelManager;
@@ -23,13 +26,47 @@ public class Main {
 		EObject root = manager.loadModel("model_input/example.sct");
 		
 		// Reading model
+		
 		Statechart s = (Statechart) root;
 		TreeIterator<EObject> iterator = s.eAllContents();
+		ArrayList<String> names = new ArrayList<String>();
+		System.out.println("Transitions:");
 		while (iterator.hasNext()) {
 			EObject content = iterator.next();
 			if(content instanceof State) {
 				State state = (State) content;
-				System.out.println(state.getName());
+				if (!state.getName().isEmpty()) {
+					names.add(state.getName());
+				}
+				
+				
+			} else if(content instanceof Transition) {
+				Transition t = (Transition) content;
+				System.out.println(t.getSource().getName() + " -> " + t.getTarget().getName());
+			}
+		}
+		System.out.println("\nStates:");
+		iterator = s.eAllContents();
+		while (iterator.hasNext()) {
+			EObject content = iterator.next();
+			if(content instanceof State) {
+				State state = (State) content;
+				String extra="";
+				if (state.getName().isEmpty()) {
+					int index = 0;
+					String proposed_name;
+					while (true) {
+						proposed_name = "State" + index;
+						if (!names.contains(proposed_name)) {
+							break;
+						}
+					}
+					extra +=" , Empty name! Suggestion: " + proposed_name;
+				}
+				if (state.getOutgoingTransitions().size() == 0) {
+					extra += ", Csapda!";
+				}
+				System.out.println("Name: " + state.getName() + extra);
 			}
 		}
 		
